af380cd69f1951f3d1e7057b5a0cbe03788ceb80
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-04 07:57:17 -0300

    Implementei o método para deletar o user pelo seu email

--- src/main/java/com/caua/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/caua/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -2,11 +2,15 @@ package com.caua.cadastro_usuario.infrastructure.repository;
 
 import com.caua.cadastro_usuario.infrastructure.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
     Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
 // Coloco o tipo que seria usuário e o tipo do meu id
