8a917cc698b6220e4732c96ff0549c1ac0cf7d12
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-04 08:28:30 -0300

    Implementei o Put

--- src/main/java/com/caua/cadastro_usuario/controller/UsuarioController.java
+++ src/main/java/com/caua/cadastro_usuario/controller/UsuarioController.java
@@ -29,4 +29,10 @@ public class UsuarioController {
         usuarioService.deletarUsuarioPorEmail(email);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Integer id, @RequestBody Usuario usuario) {
+        usuarioService.atualizarUsuarioPorId(id, usuario);
+        return ResponseEntity.ok().build();
+    }
 }
