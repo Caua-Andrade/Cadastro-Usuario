7190e0418cedd74ca5be1ea5ce10cd8b6a1843f1
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-03 21:03:37 -0300

    Terminei de implementar o usuário

--- src/main/java/com/caua/cadastro_usuario/infrastructure/entities/Usuario.java
+++ src/main/java/com/caua/cadastro_usuario/infrastructure/entities/Usuario.java
@@ -1,4 +1,25 @@
 package com.caua.cadastro_usuario.infrastructure.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    // o unique mostra que não pode ter dois usuários com o mesmo email
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
 }
